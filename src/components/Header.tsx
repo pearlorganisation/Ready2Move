2f29f8560dfaa161e1ffdfef7259649ac95f20ea
Ranjan <ranjan@pearlorganisation.com>
2025-05-02 12:02:32 +0530

    new logo added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import logo from "../assets/logo.png";
+import logo from "../assets/mainlogo.jpg";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks/dispatchHook";
 import { logoutUser } from "@/lib/redux/slice/userSlice";
@@ -17,16 +17,16 @@ const Header = () => {
 console.log("is logged in", isLoggedIn)
   return (
     <header className="sticky top-0 left-0 w-full bg-white shadow-md z-50">
-      <nav className="max-w-screen-xl mx-auto px-4 py-3 flex justify-between items-center">
+      <nav className="max-w-screen-xl mx-auto px-4 py-1 flex justify-between items-center">
         <Link href="/" className="flex items-center space-x-3">
           <Image
             src={logo}
-            className="h-8"
+            className="h-16"
             alt="Ready2Move Logo"
-            width={32}
-            height={32}
+            width={100}
+            height={100}
           />
-          <span className="text-xl font-bold text-gray-900">Ready2Move</span>
+          {/* <span className="text-xl font-bold text-gray-900">Ready2Move</span> */}
         </Link>
         <input type="checkbox" id="menu-toggle" className="hidden peer" />
         <label
