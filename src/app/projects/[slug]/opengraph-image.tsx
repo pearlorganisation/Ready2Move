5ea69908d3a5a3761cd873b6f52cf7c3299385e4
Ranjan <ranjan@pearlorganisation.com>
2025-05-12 13:54:31 +0530

    testing og image for each page

---
+++ src/app/projects/[slug]/opengraph-image.tsx
@@ -0,0 +1,34 @@
+import { axiosInstance } from '@/lib/constants/axiosInstance'
+import { ImageResponse } from 'next/og'
+ 
+export const alt = 'About Acme'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+export const contentType = 'image/png'
+ 
+export default async function Image({ params }: { params: { slug: string } }) {
+  const { data: post } = await axiosInstance.get(`/api/v1/projects/${params.slug}`)
+ 
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {post?.title}
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
