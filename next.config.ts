c4fce9611344592cba80d7bcf4e067399a8b8aed
Ranjan <ranjan@pearlorganisation.com>
2025-05-12 15:42:30 +0530

    added the og image to fetch by the

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,33 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images:{
+//     remotePatterns: [
+//       {
+//          protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//       }
+//     ]
+//   },
+  
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
+  images: {
     remotePatterns: [
       {
-         protocol: 'https',
+        protocol: 'https',
         hostname: 'res.cloudinary.com',
       }
     ]
-  }
+  },
+  // Required for Canvas to work with Next.js
+   serverExternalPackages: ['@napi-rs/canvas'],
+
 };
 
 export default nextConfig;
