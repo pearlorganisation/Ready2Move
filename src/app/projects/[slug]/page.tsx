a05a9967abfa6cb1a729129560c3cf70d49cf785
Ranjan <ranjan@pearlorganisation.com>
2025-05-12 17:07:46 +0530

    fixing errror

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,40 +1,40 @@
 
-import type { Metadata } from 'next';
+// import type { Metadata } from 'next';
 import MySlugComp from "@/components/MySlugComp";
 
-export async function generateMetadata(params:{
-  params:Promise<{slug: string}>
-})  {
-  // Fetch project data for metadata
-  // const slug = await params.slug
-  const { slug } = await params.params;
-  const res = await fetch(`https://ready2move.co.in/api/v1/projects/${slug}`);
-  const project = (await res.json())?.data;
+// export async function generateMetadata(params:{
+//   params:Promise<{slug: string}>
+// })  {
+//   // Fetch project data for metadata
+//   // const slug = await params.slug
+//   const { slug } = await params.params;
+//   const res = await fetch(`https://ready2move.co.in/api/v1/projects/${slug}`);
+//   const project = (await res.json())?.data;
 
-  return {
-    title: project?.title || 'Project Details',
-    description: project?.description || 'View project details on Ready2Move',
-    openGraph: {
-      title: project?.title || 'Ready2Move Project',
-      description: project?.description || '',
-      images: [
-        {
-          url: `https://ready2move.co.in/projects/${slug}/opengraph-image`,
-          width: 1200,
-          height: 630,
-        },
-      ],
-      url: `https://ready2move.co.in/projects/${slug}`,
-      type: 'website',
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: project?.title || 'Ready2Move Project',
-      description: project?.description || '',
-      images: [`https://ready2move.co.in/projects/${slug}/opengraph-image`],
-    },
-  };
-}
+//   return {
+//     title: project?.title || 'Project Details',
+//     description: project?.description || 'View project details on Ready2Move',
+//     openGraph: {
+//       title: project?.title || 'Ready2Move Project',
+//       description: project?.description || '',
+//       images: [
+//         {
+//           url: `https://ready2move.co.in/projects/${slug}/opengraph-image`,
+//           width: 1200,
+//           height: 630,
+//         },
+//       ],
+//       url: `https://ready2move.co.in/projects/${slug}`,
+//       type: 'website',
+//     },
+//     twitter: {
+//       card: 'summary_large_image',
+//       title: project?.title || 'Ready2Move Project',
+//       description: project?.description || '',
+//       images: [`https://ready2move.co.in/projects/${slug}/opengraph-image`],
+//     },
+//   };
+// }
 
 
 export default async function ProjectDetails(params:{
@@ -49,3 +49,4 @@ export default async function ProjectDetails(params:{
   );
 }
 
+ 
