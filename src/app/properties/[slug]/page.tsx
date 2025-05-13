32da238fe96218d26d216727051508605be7313a
Ranjan <ranjan@pearlorganisation.com>
2025-05-13 11:25:37 +0530

    testing og image for properties

--- src/app/properties/[slug]/page.tsx
+++ src/app/properties/[slug]/page.tsx
@@ -1,8 +1,40 @@
 import MySlugProperty from "@/components/MySlugProperty";
+import { getSinglePropertyBySlug } from "@/lib/serverActions/propertyOg";
+ import type { Metadata } from 'next';
 
-// interface Props {
-//   params: { slug: string };
-// }
+ // You must create this or import from wherever you fetch property details.
+
+export async function generateMetadata(params: {
+  params: Promise<{ slug: string }>
+}): Promise<Metadata> {
+ const { slug } = await params.params;
+  // Fetch property details from your backend or API using the slug
+  const property = await getSinglePropertyBySlug(slug);
+  console.log("the property is", property)
+  if (!property) {
+    return {
+      title: "Property Not Found",
+    };
+  }
+
+  return {
+    title: property.title,
+    description: property.description,
+    openGraph: {
+      title: property.title,
+      description: property.description,
+      images: [
+        {
+          url: property?.imageGallery?.[0]?.secure_url || "https://example.com/default.jpg", // Make sure the URL is absolute
+          width: 1200,
+          height: 630,
+          alt: property.title,
+        },
+      ],
+      type: "website",
+    },
+  };
+}
 
 export default async function ProjectDetails(params:{
   params:Promise<{slug:string}>
