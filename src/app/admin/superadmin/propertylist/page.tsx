e93c198f91ca1902f612dcfeeb14b01be051c59e
Anjali Bartwal <anjalibartwal@pearlorganisation.com>
2025-06-25 13:00:14 +0530

    fix the bugs

--- src/app/admin/superadmin/propertylist/page.tsx
+++ src/app/admin/superadmin/propertylist/page.tsx
@@ -1,6 +1,4 @@
-
-"use client"
-
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
@@ -8,8 +6,10 @@ import { getAllProjects } from "@/lib/redux/actions/projectAction";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks/dispatchHook";
 import PaginationMainComponent from "@/components/PaginationMain";
 import { Delete } from "lucide-react";
-import { deleteProperty, getAllProperties } from "@/lib/redux/actions/propertyAction";
-
+import {
+  deleteProperty,
+  getAllProperties,
+} from "@/lib/redux/actions/propertyAction";
 
 import { useRouter } from "next/navigation"; // ✅ App Router
 import DeleteModal from "@/components/DeletedModal";
@@ -17,49 +17,62 @@ import DeleteModal from "@/components/DeletedModal";
 const ProjectListing = () => {
   const dispatch = useAppDispatch();
   const router = useRouter();
-  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [loading, setLoading] = useState(true);
-  const { propertyData, paginate } = useAppSelector((state) => state.property)
- const[isOpenModal,setOpenModal]=useState(false)
- const[selectedData,setseletedData]=useState<string|null>(null)
-  console.log("propertyData", propertyData)
- const totalPages = Math.ceil(paginate?.total/paginate?.limit)
- const limit = paginate?.limit
-const handlePageClick = (page:number)=>{
-  if(page >0 && page < totalPages){
-      setCurrentPage(page)
-  }
-}
+  const { propertyData, paginate } = useAppSelector((state) => state.property);
+  const [isOpenModal, setOpenModal] = useState(false);
+  const [selectedData, setseletedData] = useState<string | null>(null);
+  console.log("propertyData", propertyData);
+  const totalPages = Math.ceil(paginate?.total / paginate?.limit);
+  const limit = paginate?.limit;
+  const handlePageClick = (page: number) => {
+    if (page > 0 && page < totalPages) {
+      setCurrentPage(page);
+    }
+  };
 
- 
-
-
-const handleModalOpen = (slug: string) => {
-  console.log("slug", slug);
-  router.push(`/admin/superadmin/property/edit/${slug}`);
-};
+  const handleModalOpen = (slug: string) => {
+    console.log("slug", slug);
+    router.push(`/admin/superadmin/property/edit/${slug}`);
+  };
   useEffect(() => {
-    dispatch(getAllProperties({page:currentPage, limit:50,priceRange:0 ,bedRooms:0, bathRooms:0}))
-  }, [dispatch, currentPage])
+    dispatch(
+      getAllProperties({
+        page: currentPage,
+        limit: 50,
+        priceRange: 0,
+        bedRooms: 0,
+        bathRooms: 0,
+      })
+    );
+  }, [dispatch, currentPage]);
 
- const handleDelete=(id:string)=>{
-  setseletedData(id)
-  setOpenModal(true)
- }
-  
- const confirmDelete=()=>{
-  if(selectedData){
-    dispatch(deleteProperty(selectedData)).then((res:any)=>{
-   if(res.payload.data.success){
-    dispatch(getAllProperties({page:currentPage, limit:50,priceRange:0 ,bedRooms:0, bathRooms:0}))
-    setOpenModal(false)
-   }
-    })
-  }
- }
- const closeModal=()=>{
-setOpenModal(false)
- }
+  const handleDelete = (id: string) => {
+    setseletedData(id);
+    setOpenModal(true);
+  };
+
+  const confirmDelete = () => {
+    if (selectedData) {
+      dispatch(deleteProperty(selectedData)).then((res: any) => {
+        if (res.payload.data.success) {
+          dispatch(
+            getAllProperties({
+              page: currentPage,
+              limit: 50,
+              priceRange: 0,
+              bedRooms: 0,
+              bathRooms: 0,
+            })
+          );
+          setOpenModal(false);
+        }
+      });
+    }
+  };
+  const closeModal = () => {
+    setOpenModal(false);
+  };
   return (
     <div className="p-4 overflow-x-auto">
       <h2 className="text-2xl font-bold mb-4">All Property</h2>
@@ -69,73 +82,93 @@ setOpenModal(false)
             <th className="p-3 border">S No.</th>
             <th className="p-3 border">Title</th>
             <th className="p-3 border">Image</th>
-            <th className="p-3 border text-nowrap">Property Type</th>        
+            <th className="p-3 border text-nowrap">Property Type</th>
             <th className="p-3 border">Locality</th>
             <th className="p-3 border">RERA Number</th>
             <th className="p-3 border">Services</th>
             <th className="p-3 border">Featured</th>
-          
+
             <th className="p-3 border">Actions</th>
           </tr>
         </thead>
         <tbody>
-          {
-            Array.isArray(propertyData)&& propertyData?.map((project: any,index:any) => (
+          {Array.isArray(propertyData) &&
+            propertyData?.map((project: any, index: any) => (
               <tr key={project?._id} className="border-t">
                 <td className="p-3 border">{index + 1}</td>
                 <td className="p-3 border">{project?.title}</td>
-                <td className="p-3 border">{project.imageGallery.slice(0,1).map((img:any)=>{
-                  return <img key={img} src={img.secure_url} alt="image" className="w-20 h-20 rounded-md" />
-                })}</td> 
+                <td className="p-3 border">
+                  {project.imageGallery.slice(0, 1).map((img: any) => {
+                    return (
+                      <img
+                        key={img}
+                        src={img.secure_url}
+                        alt="image"
+                        className="w-20 h-20 rounded-md"
+                      />
+                    );
+                  })}
+                </td>
                 <td className="p-3 border">{project?.propertyType?.name}</td>
-                <td className="p-3 border">{project.city} {project.locality} {project.state}</td>
-                <td className="p-3 border">{project?.reraNumber}</td>         
-                <td className="p-3 border">{project?.service}</td>  
-                  
+                <td className="p-3 border">
+                  {project.city} {project.locality} {project.state}
+                </td>
+                <td className="p-3 border">{project?.reraNumber}</td>
+                <td className="p-3 border">{project?.service}</td>
 
                 <td className="p-3 border">
                   {project.isFeatured ? (
-                    <span className="px-2 py-1 text-xs rounded bg-green-500 text-white">YES</span>
+                    <span className="px-2 py-1 text-xs rounded bg-green-500 text-white">
+                      YES
+                    </span>
                   ) : (
-                    <span className="px-2 py-1 text-xs rounded bg-gray-400 text-white">NO</span>
+                    <span className="px-2 py-1 text-xs rounded bg-gray-400 text-white">
+                      NO
+                    </span>
                   )}
                 </td>
-              
-                <td className="p-3 flex justify-center items-center mt-6 gap-4">
-                  <button className="bg-yellow-400 p-2 rounded text-white hover:bg-yellow-500" onClick={()=>handleModalOpen(project.slug)}>
 
+                {/* fdsmnhfdngm,dfn,ngm,dnm,ndsm,nm,dsn,sdn */}
+
+                <td className="p-3 flex justify-center items-center mt-6 gap-4">
+                  <button
+                    className="bg-yellow-400 p-2 rounded text-white hover:bg-yellow-500"
+                    onClick={() => handleModalOpen(project.slug)}
+                  >
                     <FaEdit />
                   </button>
-                  <button className="bg-red-500 p-2 rounded text-white hover:bg-red-600" onClick={()=>{
-                    handleDelete(project._id)
-                  }}>
+                  <button
+                    className="bg-red-500 p-2 rounded text-white hover:bg-red-600"
+                    onClick={() => {
+                      handleDelete(project._id);
+                    }}
+                  >
                     <FaTrash />
                   </button>
                 </td>
               </tr>
-            )
-          )}
+            ))}
         </tbody>
       </table>
-      
+
       <div className="mt-12 flex justify-center">
-     <PaginationMainComponent
-      totalPages={totalPages}
-      currentPage={currentPage}
-      paginate={paginate}
-      handlePageClick={handlePageClick}
-     /></div>
-    
-     <div>
-    
-     </div>
-{
-isOpenModal&&(
- <DeleteModal isOpen={isOpenModal} onClose={closeModal} onDelete={confirmDelete}/>)}
+        <PaginationMainComponent
+          totalPages={totalPages}
+          currentPage={currentPage}
+          paginate={paginate}
+          handlePageClick={handlePageClick}
+        />
+      </div>
 
-
-   </div>
-   
+      <div></div>
+      {isOpenModal && (
+        <DeleteModal
+          isOpen={isOpenModal}
+          onClose={closeModal}
+          onDelete={confirmDelete}
+        />
+      )}
+    </div>
   );
 };
 
